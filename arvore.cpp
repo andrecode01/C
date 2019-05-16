98e5d13745a5261e38ffa2d9dac1b03c30ca331b
andrecode01 <andrecorradi00@gmail.com>
2019-05-16 20:45:16 -0300

    Update arvore.cpp

--- arvore.cpp
+++ arvore.cpp
@@ -48,6 +48,14 @@ int main(int argc, char** argv) {
 	} while (opcao != 0);
 	
 	printf("\n\tID da Raiz: %d", arvore->raiz->no_id);
+	printf("\n\tID da RaizDIR: %d", arvore->raiz->no_dir->no_id);
+	printf("\n\tID da RaizESQ: %d", arvore->raiz->no_esq->no_id);
+	
+	printf("\n\tID da RaizDIR-FILHO-DIR %d", arvore->raiz->no_dir->no_dir->no_id);
+
+	
+	
+	
 
 	return 0;
 }
@@ -85,17 +93,32 @@ void inserir_nos(t_arvore *arvore, int valorB){
 		arvore->no_temp = arvore->raiz;
 
 	} 
-  else{
+  	else{
 
     arvore->no_temp = arvore->raiz;
 
     int valorBtemp;
-
+    
     valorB = no_bot->no_id;
 
     do{
 
       valorBtemp = busca(arvore, valorB);
+	  
+	  if(valorBtemp == 1){
+	  	
+	  	arvore->no_temp->no_dir = no_bot;
+	  	valorBtemp = 0;
+	  	
+	  }
+	  
+	  if(valorBtemp == 2){
+	  	
+	  	arvore->no_temp->no_esq = no_bot;
+	  	valorBtemp = 0;
+	  	
+	  }
+	    
 
     }while(valorBtemp != 0);
 
@@ -104,11 +127,31 @@ void inserir_nos(t_arvore *arvore, int valorB){
 
 int busca(t_arvore *arvore, int valorB){
 
-  if(valorB > arvore->no_temp->no_id){
-
-    
-
-  }
-
-
+	if(valorB > arvore->no_temp->no_id){
+		
+		if(arvore->no_temp->no_dir == NULL)
+		return 1;
+		else{
+		arvore->no_temp = arvore->no_temp->no_dir;
+		return 100;
+		}	
+		
+	} else{
+		
+		if(arvore->no_temp->no_esq == NULL)
+		return 2;
+		else{
+		arvore->no_temp = arvore->no_temp->no_esq;
+		return 100;
+		}
+		
+	}
 }
+
+
+
+
+
+
+
+
